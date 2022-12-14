c48457f39bcfdd9de1a9dc44e5d5757ecda7cb49
Nikita <medvedhopod@gmail.com>
2022-12-14 16:10:23 +0200

    omn2 GO

--- omn2.py
+++ omn2.py
@@ -7,9 +7,13 @@ n = np.arange(N)
 x = np.sin(2 * np.pi * n * 0.03)
 x_noisy = x + 0.1 * np.random.randn(N)
 
-polyFunction = np.polyfit(x_noisy, n, 3)
-print(polyFunction)
-
+plt.subplot(2, 1, 1)
 plt.plot(n, x)
 plt.plot(n, x_noisy)
+
+poLyFunc = np.poly1d(np.polyfit(n, x_noisy, 3))
+
+plt.subplot(2, 1, 2)
+plt.plot(n, poLyFunc(n), 'b')
+plt.plot(n, x_noisy, 'r')
 plt.show()
