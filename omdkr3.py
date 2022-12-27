6d7752ce745135fa2a7de3611ddf9a8c1f7c5e76
Nikita <medvedhopod@gmail.com>
2022-12-27 23:20:15 +0200

    omdkr3

--- omdkr3.py
+++ omdkr3.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
-from random import gauss
+import cv2
 
 
 def gaussNoiseImg(img):
@@ -36,20 +36,34 @@ def forSaltPaper(img, noise_img, i):
     d = 0.1 + (10 - 14) / 100
     num_salt = np.ceil(d * img.size * 0.5)
     x_y = [np.random.randint(0, i - 1, int(num_salt)) for i in img.shape]
-    noise_img[x_y] = i
+    noise_img[tuple(x_y)] = i
     return noise_img
 
 
-def mean_filter(img):
-    n = 3
-    clean_img = mahotas.mean_filter(img, n)
-    plt.imshow(clean_img)
-    plt.title(f"Rezult, Average {n}x{n}")
-    plt.show()
+def mean_filter():
+    for size in [3, 5, 7, 11]:
+        image = cv2.imread('noise.png')
+        mean = cv2.blur(image, (size, size))
+        plt.imshow(mean)
+        plt.title(f"Mean filter {size}x{size}")
+        plt.show()
+
+
+def median_filter():
+    for size in [3, 5, 7, 11]:
+        image = cv2.imread('noise.png')
+        median = cv2.medianBlur(image, size)
+        plt.imshow(median)
+        plt.title(f"Median filter {size}x{size}")
+        plt.show()
 
 
 if __name__ == "__main__":
     gaussImg = gaussNoiseImg(plt.imread('img.png'))
     multiImg = multiNoiseImg(gaussImg)
     saltPepperImg = saltPepper(multiImg)
-    mean_filter(saltPepper)
+
+    plt.imsave('noise.png', saltPepperImg)
+
+    mean_filter()
+    median_filter()
