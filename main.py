64ba9bb7a9f5c225cc7b65f0d99a77ed9ffd0be8
Nikita <medvedhopod@gmail.com>
2023-04-27 16:36:30 +0300

    corrected again

--- main.py
+++ main.py
@@ -9,23 +9,27 @@ import matplotlib
 matplotlib.use('TkAgg')
 
 
-def find_support_vectors(svm_reg, X, y):
+def find_support_vectors(svm_reg):
+    """Red marks the points that go beyond the support vectors"""
+
     y_pred = svm_reg.predict(X)
     epsilon = svm_reg[-1].epsilon
     off_margin = np.abs(y - y_pred) >= epsilon
     return np.argwhere(off_margin)
 
 
-def plot_svm_regression(svm_reg_fun, x_fun, y_fun, axes_fun):
-    support_vectors = find_support_vectors(svm_reg_fun, x_fun, y_fun)
+def plot_svm_regression(svm_reg_fun, axes_fun):
+    """Displays set points and draws vectors"""
+
+    support_vectors = find_support_vectors(svm_reg_fun)
     x1s = np.linspace(axes_fun[0], axes_fun[1], 100).reshape(100, 1)
     y_pred = svm_reg_fun.predict(x1s)
     epsilon = svm_reg_fun[-1].epsilon
     plt.plot(x1s, y_pred, "k-", linewidth=2, label=r"$\hat{y}$", zorder=-2)
     plt.plot(x1s, y_pred + epsilon, "k--", zorder=-2)
     plt.plot(x1s, y_pred - epsilon, "k--", zorder=-2)
-    plt.scatter(x_fun[support_vectors], y_fun[support_vectors], s=180, facecolors='red', zorder=-1)
-    plt.plot(x_fun, y_fun, "bo")
+    plt.scatter(X[support_vectors], y[support_vectors], s=180, facecolors='red', zorder=-1)
+    plt.plot(X, y, "bo")
     plt.plot(x1s, y_pred)
     plt.xlabel("$x_1$")
     plt.legend(loc="upper left")
@@ -59,13 +63,13 @@ if __name__ == "__main__":
     fig, axes = plt.subplots(ncols=2, figsize=(9, 4), sharey='all')
 
     plt.sca(axes[0])
-    plot_svm_regression(svm_linear_reg, X, y, [-1.5, 1.5, -0.4, 1])
+    plot_svm_regression(svm_linear_reg, [-1.5, 1.5, -0.4, 1])
     plt.title(f"C={svm_linear_reg[-1].C}, "
               f"epsilon={svm_linear_reg[-1].epsilon}")
     plt.grid()
 
     plt.sca(axes[1])
-    plot_svm_regression(svm_poly_reg, X, y, [-1.5, 1.5, -0.4, 1])
+    plot_svm_regression(svm_poly_reg, [-1.5, 1.5, -0.4, 1])
     plt.title(f"degree={svm_poly_reg[-1].degree}, "
               f"C={svm_poly_reg[-1].C}, "
               f"epsilon={svm_poly_reg[-1].epsilon}")
@@ -81,17 +85,17 @@ if __name__ == "__main__":
     svm_sigmoid_reg = make_pipeline(StandardScaler(), SVR(kernel="sigmoid", coef0=0, C=3, gamma=0.2, epsilon=0.3))
     svm_sigmoid_reg.fit(X, y)
 
-    fig2, axes = plt.subplots(ncols=2, figsize=(9, 4), sharey=True)
+    fig2, axes = plt.subplots(ncols=2, figsize=(9, 4), sharey='all')
 
     plt.sca(axes[0])
-    plot_svm_regression(svm_rbf_reg, X, y, [-1.5, 1.5, -0.4, 1])
+    plot_svm_regression(svm_rbf_reg, [-1.5, 1.5, -0.4, 1])
     plt.title(f"C={svm_rbf_reg[-1].C}, "
               f"gamma={svm_rbf_reg[-1].gamma}, "
               f"epsilon={svm_rbf_reg[-1].epsilon}")
     plt.grid()
 
     plt.sca(axes[1])
-    plot_svm_regression(svm_sigmoid_reg, X, y, [-1.5, 1.5, -0.4, 1])
+    plot_svm_regression(svm_sigmoid_reg, [-1.5, 1.5, -0.4, 1])
     plt.title(f"coef0={svm_sigmoid_reg[-1].coef0}, "
               f"C={svm_sigmoid_reg[-1].C}, "
               f"gamma={svm_sigmoid_reg[-1].gamma}, "
